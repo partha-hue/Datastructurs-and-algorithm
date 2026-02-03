b92c837f6bcf2df03274b8f8c1ee9646d276e31f
Partha <parthachakraborty383@gmail.com>
2026-02-03 22:54:56 +0530

    add list new oparetion

---
+++ list sroting.py
@@ -0,0 +1,20 @@
+def removeduplicates(arr):
+    seen = set()
+
+    idx = 0
+
+    for i in range(len(arr)):
+        if arr[i] not in seen:
+            seen.add(arr[i])
+            arr[idx] = arr[i]
+            idx += 1
+
+    return idx
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 2, 3, 4, 4, 4, 5, 5]
+    newsize = removeduplicates(arr)
+
+    for i in range(newsize):
+        print(arr[i], end=" ")
